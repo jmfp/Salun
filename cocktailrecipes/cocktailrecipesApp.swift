eaf203e50f0cf0467de28ebaa32b5014e5cea574
jmfp <therealjmfp@gmail.com>
2024-03-10 01:23:09 -0500

    Initial Commit

---
+++ cocktailrecipes/cocktailrecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocktailrecipesApp.swift
+//  cocktailrecipes
+//
+//  Created by Jesse Price on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct cocktailrecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
